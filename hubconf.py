ee843445d0346c2939e82367d5857274a979c3e0
grez72 <grez72@gmail.com>
2022-06-20 10:15:30 -0400

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -13,6 +13,7 @@ _doc ="""Official ipcl imagenet model from the paper `A self-supervised domain-g
     
     Args:
         pretrained (bool): whether to load pre-trained weights
+        
     returns: model, transform
         model: the requested model
         transform: the validation transforms needed to pre-process images
@@ -36,18 +37,9 @@ def _transform(resize=256, crop_size=224, mean=[0.485, 0.456, 0.406], std=[0.229
     
     return transform
 
+@_docstring_parameter(_doc.format("Ref#1","(alexnet_gn trained on imagenet with instance-prototype-contrastive learning; IPCL)"))
 def alexnetgn_ipcl_ref1(pretrained=True, **kwargs):
-    """Official ipcl imagenet model from the paper `A self-supervised domain-general learning framework for human ventral stream representation <https://github.com/grez72/publications/blob/master/pdfs/Konkle_et_al-2022-Nature_Communications.pdf>`.    
-    
-    This model instance corresponds to Supplementary Table Ref#1 (alexnet_gn trained on imagenet with instance-prototype-contrastive learning; IPCL).
-    
-    Args:
-        pretrained (bool): whether to load pre-trained weights
-    returns: model, transform
-        model: the requested model
-        transform: the validation transforms needed to pre-process images
-        
-    """
+    """{0}"""
     
     model = _alexnet_gn(out_dim=128, l2norm=True)
           
@@ -87,6 +79,30 @@ def alexnetgn_ipcl_ref2(pretrained=True, **kwargs):
         state_dict = {str.replace(k,'module.',''): v for k,v in checkpoint['state_dict'].items()}
         model.load_state_dict(state_dict, strict=True)
         model.hashid = 'a4b3151e28'
+        model.weights_file = os.path.join(torch.hub.get_dir(), "checkpoints", cache_file_name)
+    
+    transform = _transform(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    
+    return model, transform
+
+@_docstring_parameter(_doc.format("Ref#3","(alexnet_gn trained on imagenet with instance-prototype-contrastive learning; IPCL)"))
+def alexnetgn_ipcl_ref3(pretrained=True, **kwargs):
+    """{0}"""
+    
+    model = _alexnet_gn(out_dim=128, l2norm=True, **kwargs)
+          
+    if pretrained:
+        checkpoint_name = "alexnet_gn_dim128_unsupervised_redux_checkpoint_weights_only.pth.tar"
+        cache_file_name = "alexnetgn_ipcl_ref3-543e478735.pth.tar"
+        checkpoint = torch.hub.load_state_dict_from_url(
+            url=f'https://visionlab-pretrainedmodels.s3.amazonaws.com/project_instancenet/ipcl/{checkpoint_name}', 
+            map_location='cpu',
+            file_name=cache_file_name,
+            check_hash=True
+        )
+        state_dict = {str.replace(k,'module.',''): v for k,v in checkpoint['state_dict'].items()}
+        model.load_state_dict(state_dict, strict=True)
+        model.hashid = '543e478735'
         model.weights_file = os.path.join(torch.hub.get_dir(), "checkpoints", cache_file_name)
     
     transform = _transform(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
